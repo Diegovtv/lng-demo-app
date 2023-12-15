4dd4da12449fa957062317eb28440910cad3de43
Diego Fuenmayor <diego@velope.tv>
2023-12-15 19:30:54 -0400

    Image URL fix

--- dist/es6/js/appBundle.js
+++ dist/es6/js/appBundle.js
@@ -3,7 +3,7 @@
  * SDK version: 5.5.0
  * CLI version: 2.13.0
  * 
- * Generated: Fri, 15 Dec 2023 23:28:53 GMT
+ * Generated: Fri, 15 Dec 2023 23:30:33 GMT
  */
 
 var APP_com_domain_app_ottdemoapplng = (function () {
@@ -9060,7 +9060,7 @@ once:   once$1,
           const data = await response.json();
           const cards = data.results.map((poster, idx) => ({
             type: PosterComponent,
-            posterUrl: "http://image.tmdb.org/t/p/w1280_and_h720_multi_faces".concat(poster.backdrop_path),
+            posterUrl: "https://image.tmdb.org/t/p/w1280_and_h720_multi_faces".concat(poster.backdrop_path),
             posterTitle: poster.title,
             posterDesc: poster.overview,
             posterID: poster.id,
